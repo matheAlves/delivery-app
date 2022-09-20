d77aff277ff9433a791f91de4f67c4b15fecb537
kaellAlecs <kaellaleksander@gmail.com>
2022-09-20 17:57:50 -0300

    feat: initial sequelize setup done, implemented the configurations required to run the db

---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+  }
+
+  Product.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING(100)
+    },
+    price: {
+      type: DataTypes.DECIMAL(4,2)
+    },
+    urlImage: {
+      type: DataTypes.STRING(200),
+      field: 'url_image'
+    },
+  }, {
+    sequelize,
+    modelName: 'Product',
+    timestamps: false,
+  });
+  return Product;
+};
