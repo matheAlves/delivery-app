5eb3a147ccd105712724b6adf6e32adead3a8019
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-21 19:55:07 -0300

    feat: implementação do método GET /users de teste

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const UserService= require('../service/UserService');
+const { user } = require('../database/models/index');
+const UserController = require('../controller/UserController');
 
 const app = express();
+const userService = new UserService(user);
+const userController = new UserController(userService);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/users', userController.findAll);
 
 module.exports = app;
