cd10e0840f15a1ae96604fdbf7a7f37146f0115a
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-21 19:54:51 -0300

    feat: criação do UserController

---
+++ back-end/src/controller/UserController.js
@@ -0,0 +1,15 @@
+class UserController {
+  _service;
+
+  constructor(service) {
+    this._service = service
+  }
+
+  findAll = async (_req, res) => {
+    const result = await this._service.findAll();
+
+    res.status(200).json(result);
+  }
+}
+
+module.exports = UserController;
