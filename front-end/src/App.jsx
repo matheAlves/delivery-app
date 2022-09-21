50cf4215439cb05f8bd5472f107820d561b5ee4c
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-21 17:05:28 -0300

    feat: implementação da rota /login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyProvider from './Provider/MyProvider';
-import Exemplo from './pages/Exemplo';
+import RedirectToLogin from './pages/RedirectToLogin';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <MyProvider>
         <Routes>
-          <Route path="/" element={ <Exemplo /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/" element={ <RedirectToLogin /> } />
         </Routes>
       </MyProvider>
     </BrowserRouter>
