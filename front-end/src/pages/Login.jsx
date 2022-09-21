773a3d97f3dc98941549bade195f279c11c67296
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-21 18:02:03 -0300

    feat: implementação da validação das credenciais ao fazer login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const navigate = useNavigate();
+  const [valid, setValid] = useState(false);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const MIN_PASSWORD_LENGTH = 6;
+    const loginRegex = /\S+@\S+\.com/;
+
+    if (password.length >= MIN_PASSWORD_LENGTH && loginRegex.test(login)) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  }, [login, password]);
+
   return (
     <form>
       <label htmlFor="login">
         Login
         <input
           id="login"
+          type="text"
           placeholder="email@tryber.com"
           data-testid="common_login__input-email"
+          value={ login }
+          onChange={ ({ target }) => setLogin(target.value) }
         />
       </label>
 
@@ -16,14 +36,17 @@ function Login() {
         Senha
         <input
           id="password"
-          placeholder="email@tryber.com"
+          type="password"
           data-testid="common_login__input-password"
+          value={ password }
+          onChange={ ({ target }) => setPassword(target.value) }
         />
       </label>
 
       <button
         type="button"
         data-testid="common_login__button-login"
+        disabled={ !valid }
       >
         LOGIN
       </button>
@@ -31,13 +54,16 @@ function Login() {
       <button
         type="button"
         data-testid="common_login__button-register"
+        onClick={ () => navigate('/register') }
       >
         Ainda não tenho conta
       </button>
 
-      <p data-testid="common_login__element-invalid-email">
-        Elemento oculto (mensagem de erro)
-      </p>
+      { !valid && (
+        <p data-testid="common_login__element-invalid-email">
+          Elemento oculto (mensagem de erro)
+        </p>
+      )}
     </form>
   );
 }
