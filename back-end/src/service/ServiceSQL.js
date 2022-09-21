604a8c1c5a74263f44efe9af44774cb7ede10a13
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-21 19:54:12 -0300

    feat: criação de uma classe ServiceSQL genérica

---
+++ back-end/src/service/ServiceSQL.js
@@ -0,0 +1,15 @@
+class ServiceSQL {
+  _model;
+
+  constructor(model) {
+    this._model = model;
+  }
+
+  findAll = async () => {
+    const result = await this._model.findAll();
+
+    return result;
+  }
+}
+
+module.exports = ServiceSQL;
