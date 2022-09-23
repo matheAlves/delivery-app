824727c075f37fbbcd8159ad878759d35fec6f26
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-23 19:15:07 -0300

    feat: adjustment of ClientPedidos page

--- front-end/src/pages/Client/Pedidos/ClientPedidos.jsx
+++ front-end/src/pages/Client/Pedidos/ClientPedidos.jsx
@@ -1,3 +1,4 @@
+import ItemsOrdered from '../../../Components/ItemsOrdered/ItemsOrdered';
 import UserNavbar from '../../../Components/UserNavbar/UsersNavbar';
 
 function ClientOrders() {
@@ -6,8 +7,44 @@ function ClientOrders() {
       <header>
         <UserNavbar />
       </header>
+
       <section>
-        <h1>Pedidos</h1>
+        <h2>Finalizar Pedido</h2>
+        <ItemsOrdered />
+        <h2 data-testid="customer_checkout__element-order-total-price">Total</h2>
+      </section>
+
+      <section>
+        <select data-testid="customer_checkout__select-seller">
+          <option>Vendedores</option>
+        </select>
+
+        <label htmlFor="addres">
+          Endereço
+          <input
+            id="addres"
+            data-testid="customer_checkout__input-address"
+            type="text"
+          />
+        </label>
+
+        <label htmlFor="number-addres">
+          Número
+          <input
+            id="number-addres"
+            data-testid="customer_checkout__input-address-number"
+            type="number"
+          />
+        </label>
+
+        <div>
+          <button
+            type="button"
+            data-testid="customer_checkout__button-submit-order"
+          >
+            Finalizar pedido
+          </button>
+        </div>
       </section>
     </>
   );
