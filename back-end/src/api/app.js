12e9699fdca4067eeca7866b97547a47252c59af
vnpuga <vnpuga@gmail.com>
2022-09-23 11:02:56 -0300

    feat: add route for new user registration

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,13 +4,16 @@ const cors = require('cors');
 const errorHanlder = require('../middlewares/errorHandler');
 const userRoute = require('../routes/users');
 const { productRoute } = require('../routes/productRoute');
+const registerRouter = require('../routes/register');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.use('/users', userRoute);
 app.use('/products', productRoute);
+app.use('/register', registerRouter);
 
 app.use(errorHanlder);
 
