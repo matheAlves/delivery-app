796d4ef05ff26a811ce48bac2457dada1f3c3c2e
vnpuga <vnpuga@gmail.com>
2022-09-27 16:00:02 -0300

    feat: starting implementation for order screen

---
+++ front-end/src/pages/Client/Pedidos/Orders.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import OrderCard from '../../../Components/OrderCard/OrderCard';
+import UserNavbar from '../../../Components/UserNavbar/UsersNavbar';
+
+function Orders() {
+  // const [orders, setOrders] = useState([]); // ver de colocar no provider
+
+  return (
+    <div>
+      <header>
+        <UserNavbar />
+      </header>
+      <div>
+        {orders.map((item) => (
+          <OrderCard
+            key={ item.id }
+            id={ item.id }
+            status={ item.status }
+            saleDate={ item.saleDate }
+            totalPrice={ item.totalPrice }
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
