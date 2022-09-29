c5b977253be741ec7f34f757b0a9c1ff071e7284
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-29 15:16:19 -0300

    feat: adition of some inputs

--- front-end/src/pages/Admin/Admin.jsx
+++ front-end/src/pages/Admin/Admin.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdminNavbar from '../../Components/AdminNavbar/AdminNavbar';
 
 function Admin() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
       <AdminNavbar />
-      <h1>Admin</h1>
+      <form>
+        <h2>Cadastrar novo usuário</h2>
+
+        <label htmlFor="name">
+          Nome
+          <input
+            id="name"
+            data-testid="customer_checkout__input-address"
+            type="text"
+            value={ name }
+            onChange={ ({ target: { value } }) => setName(value) }
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            data-testid="customer_checkout__input-address"
+            type="text"
+            value={ email }
+            onChange={ ({ target: { value } }) => setEmail(value) }
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha
+          <input
+            id="password"
+            data-testid="customer_checkout__input-address"
+            type="password"
+            value={ password }
+            onChange={ ({ target: { value } }) => setPassword(value) }
+          />
+        </label>
+      </form>
     </>
   );
 }
